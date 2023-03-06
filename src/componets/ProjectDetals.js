44a0ad44a8efa74a65d1e511b719efa0894a6751
ariful171042 <arif171042@gmail.com>
2023-03-06 10:37:42 +0600

    real tiome deleted

--- src/componets/ProjectDetals.js
+++ src/componets/ProjectDetals.js
@@ -1,6 +1,21 @@
 import { currencyFormatter } from "../utils/currencyFormatter";
+import { useProjectsContext } from "../hooks/useProjectContext";
 
 const ProjectDetals = ({ project }) => {
+  const { dispatch } = useProjectsContext();
+  const handleDelete = async () => {
+    const res = await fetch(
+      `http://localhost:5000/api/projects/${project._id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await res.json();
+    if (res.ok) {
+      dispatch({ type: "DELETE_PROJECT", payload: json });
+    }
+  };
+
   return (
     <div className="project bg-slate-800 p-5 rounded-xl shadow-xl border border-slate-700 flex flex-col gap-5 ">
       <div className="top">
@@ -33,7 +48,12 @@ const ProjectDetals = ({ project }) => {
         <button className="bg-sky-400 text-slate-900 py-2 px-5 rounded shadow-xl hover:bg-sky-50 duration-300">
           Update
         </button>
-        <button className="text-rose-500 hover:underline">Delete</button>
+        <button
+          onClick={handleDelete}
+          className="text-rose-500 hover:underline"
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
