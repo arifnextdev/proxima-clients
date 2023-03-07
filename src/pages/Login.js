b354ca7340a31fb4ace1b75a071399ba6e017a73
ariful171042 <arif171042@gmail.com>
2023-03-07 12:47:12 +0600

    login and signup added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { login, error, loading } = useLogin();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    //login user
+    await login(email, password);
+  };
   return (
-    <form className="login-form flex flex-col gap-5 py-20 mx-auto max-w-sm">
+    <form
+      onSubmit={handleLogin}
+      className="login-form flex flex-col gap-5 py-20 mx-auto max-w-sm"
+    >
       <h2 className="text-4xl font-medium text-sky-400 mb-10">Login</h2>
 
       <div className="form-comtrol flex flex-col gap-2 ">
@@ -35,16 +47,22 @@ const Login = () => {
           id="password"
           placeholder="Enter your password"
           value={password}
-          onChange={(e) => setPassword(e.target.password)}
+          onChange={(e) => setPassword(e.target.value)}
           className="bg-transparent border border-slate-500 py-3 px-5 rounded-xl outline-none focus:border-sky-400 duration-300"
         />
       </div>
       <button
+        disabled={loading}
         type="submit"
         className="mt-3 bg-sky-400 text-slate-900 p-3 rounded-xl hover:bg-sky-500 duration-300"
       >
         Log in
       </button>
+      {error && (
+        <p className="bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500">
+          {error}
+        </p>
+      )}
     </form>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
+import { useSignup } from "../hooks/useSignup";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { signup, error, loading } = useSignup();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+
+    //signup user
+    await signup(email, password);
+  };
   return (
-    <form className="signup-form flex flex-col gap-5 py-20 mx-auto max-w-sm">
+    <form
+      onSubmit={handleSignup}
+      className="signup-form flex flex-col gap-5 py-20 mx-auto max-w-sm"
+    >
       <h2 className="text-4xl font-medium text-sky-400 mb-10">Sign up</h2>
 
       <div className="signup-comtrol flex flex-col gap-2 ">
@@ -35,16 +47,22 @@ const Signup = () => {
           id="password"
           placeholder="Enter your password"
           value={password}
-          onChange={(e) => setPassword(e.target.password)}
+          onChange={(e) => setPassword(e.target.value)}
           className="bg-transparent border border-slate-500 py-3 px-5 rounded-xl outline-none focus:border-sky-400 duration-300"
         />
       </div>
       <button
+        disabled={loading}
         type="submit"
         className="mt-3 bg-sky-400 text-slate-900 p-3 rounded-xl hover:bg-sky-500 duration-300"
       >
         Sign up
       </button>
+      {error && (
+        <p className="bg-rose-500/20 rounded-lg p-5 text-rose-500 border border-rose-500">
+          {error}
+        </p>
+      )}
     </form>
   );
 };
