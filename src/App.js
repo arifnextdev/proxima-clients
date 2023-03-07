295a6721c75c2966fa6d6d90449f77b40ef51914
ariful171042 <arif171042@gmail.com>
2023-03-07 08:33:39 +0600

    login and signup routs and style added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Footer from "./componets/Footer";
 import Navber from "./componets/Navber";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Navber />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <Footer />
     </div>
--- src/componets/Navber.js
+++ src/componets/Navber.js
@@ -2,10 +2,21 @@ import { Link } from "react-router-dom";
 
 const Navber = () => {
   return (
-    <div className="navber container mx-auto h-20 flex items-center border-b border-sky-900 ">
+    <div className="navber container mx-auto h-20 flex justify-between items-center border-b border-sky-900 ">
       <Link to="/" className="logo text-2xl font-medium text-sky-400">
         Proxima
       </Link>
+
+      <nav className="navber flex gap-5">
+        <Link to="/login " className="hover:text-sky-400 duration-300">
+          {" "}
+          Login{" "}
+        </Link>
+        <Link to="/signup" className="hover:text-sky-400 duration-300">
+          {" "}
+          Signup{" "}
+        </Link>
+      </nav>
     </div>
   );
 };
